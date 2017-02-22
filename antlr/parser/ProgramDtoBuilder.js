f22270016577595784dfd3b8b6628f5a6265a103
harry-tallbelt <harry-tallbelt@users.noreply.github.com>
2017-02-23 00:03:33 +0200

    Got rid of console.log's

--- antlr/parser/ProgramDtoBuilder.js
+++ antlr/parser/ProgramDtoBuilder.js
@@ -14,13 +14,11 @@ ProgramDtoBuilder.prototype.visitProgram = function(ctx) {
   // which does happen for an empty program.
   // This is a bug. `children` should probably be an empty array.
   // So do not replace `ctx.children` with `ctx.statements()`.
-  // console.log(ctx.children)
   return { statements: ctx.children !== null ? this.visit(ctx.statements()) : [] }
 }
 
 
 ProgramDtoBuilder.prototype.visitStatements = function(ctx) {
-  // console.log('Statements')
   return ctx.statement().map(sctx => {
     const statement = this.visit(sctx)
 
@@ -42,19 +40,16 @@ ProgramDtoBuilder.prototype.visitStatements = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitAbort = function(ctx) {
-  // console.log('Abort')
   return { type: 'abort' }
 }
 
 
 ProgramDtoBuilder.prototype.visitSkip = function(ctx) {
-  // console.log('Skip')
   return { type: 'skip' }
 }
 
 
 ProgramDtoBuilder.prototype.visitAssignment_statement = function(ctx) {
-  // console.log('Assignment_statement')
   const assignment = this.visit(ctx.assignment())
   assignment.type = 'assign'
   return assignment
@@ -62,7 +57,6 @@ ProgramDtoBuilder.prototype.visitAssignment_statement = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitScalar_assignment = function(ctx) {
-  // console.log('Scalar_assignment')
   return {
     lvalues: [this.visit(ctx.variable())],
     rvalues: [this.visit(ctx.int_expr())]
@@ -71,7 +65,6 @@ ProgramDtoBuilder.prototype.visitScalar_assignment = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitVector_assignment = function(ctx) {
-  // console.log('Vector_assignment')
   const assignment = this.visit(ctx.assignment())
   assignment.lvalues.unshift(this.visit(ctx.variable()))
   assignment.rvalues.push(this.visit(ctx.int_expr()))
@@ -80,7 +73,6 @@ ProgramDtoBuilder.prototype.visitVector_assignment = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitIf_statement = function(ctx) {
-  // console.log('If_statement')
   const guardedCommands = this.visit(ctx.guarded_commands())
   guardedCommands.type = 'if'
   return guardedCommands
@@ -88,7 +80,6 @@ ProgramDtoBuilder.prototype.visitIf_statement = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitDo_statement = function(ctx) {
-  // console.log('Do_statement')
   const guardedCommands = this.visit(ctx.guarded_commands())
   guardedCommands.type = 'do'
   return guardedCommands
@@ -96,7 +87,6 @@ ProgramDtoBuilder.prototype.visitDo_statement = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitGuarded_commands = function(ctx) {
-  // console.log('Guarded_commands')
   return {
     guards: ctx.guarded_command().map(gc => this.visit(gc.bool_expr())),
     commands: ctx.guarded_command().map(gc => this.visit(gc.statements()))
@@ -105,7 +95,6 @@ ProgramDtoBuilder.prototype.visitGuarded_commands = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitInt_const_expr = function(ctx) {
-  // console.log('Int_const_expr')
   return {
     type: 'const',
     negated: ctx.MINUS() != null,
@@ -115,7 +104,6 @@ ProgramDtoBuilder.prototype.visitInt_const_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitVariable_expr = function(ctx) {
-  // console.log('Variable_expr')
   return {
     type: 'var',
     negated: ctx.MINUS() != null,
@@ -125,7 +113,6 @@ ProgramDtoBuilder.prototype.visitVariable_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitParet_int_expr = function(ctx) {
-  // console.log('Paret_int_expr')
   return {
     type: 'parets',
     negated: ctx.MINUS() != null,
@@ -135,7 +122,6 @@ ProgramDtoBuilder.prototype.visitParet_int_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitMult_expr = function(ctx) {
-  // console.log('Mult_expr')
   return {
     type: 'mult',
     left: this.visit(ctx.int_expr(0)),
@@ -145,7 +131,6 @@ ProgramDtoBuilder.prototype.visitMult_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitAdd_expr = function(ctx) {
-  // console.log('Add_expr')
   return {
     type: ctx.PLUS() ? 'plus' : 'minus',
     left: this.visit(ctx.int_expr(0)),
@@ -155,7 +140,6 @@ ProgramDtoBuilder.prototype.visitAdd_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitBool_const_expr = function(ctx) {
-  // console.log('Bool_const_expr')
   return {
     type: 'const',
     negated: ctx.NEGATION() != null,
@@ -165,7 +149,6 @@ ProgramDtoBuilder.prototype.visitBool_const_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitParet_bool_expr = function(ctx) {
-  // console.log('Paret_bool_expr')
   return {
     type: 'parets',
     negated: ctx.NEGATION() != null,
@@ -175,7 +158,6 @@ ProgramDtoBuilder.prototype.visitParet_bool_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitAnd_expr = function(ctx) {
-  // console.log('And_expr')
   return {
     type: 'and',
     leftBool: this.visit(ctx.bool_expr(0)),
@@ -185,7 +167,6 @@ ProgramDtoBuilder.prototype.visitAnd_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitOr_expr = function(ctx) {
-  // console.log('Or_expr')
   return {
     type: 'or',
     leftBool: this.visit(ctx.bool_expr(0)),
@@ -195,7 +176,6 @@ ProgramDtoBuilder.prototype.visitOr_expr = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitComparison_expr = function(ctx) {
-  // console.log('Comparison_expr')
   return {
     type: 'comp',
     leftInt: this.visit(ctx.int_expr(0)),
@@ -213,7 +193,6 @@ ProgramDtoBuilder.prototype.visitNeq = ctx => '<>'
 
 
 ProgramDtoBuilder.prototype.visitVariable = function(ctx) {
-  // console.log('Variable')
   return {
     name: ctx.NAME().getText(),
     selectors: ctx.selectors()
@@ -224,7 +203,6 @@ ProgramDtoBuilder.prototype.visitVariable = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitSelectors = function(ctx) {
-  // console.log('Selectors')
   return ctx.selector()
     ? ctx.selector().map(sctx => this.visit(sctx))
     : []
@@ -232,7 +210,6 @@ ProgramDtoBuilder.prototype.visitSelectors = function(ctx) {
 
 
 ProgramDtoBuilder.prototype.visitSelector = function(ctx) {
-  // console.log('Selector')
   return this.visit(ctx.int_expr())
 }
 
